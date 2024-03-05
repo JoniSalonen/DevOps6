768b3df6fac4c88b626a053f1027c1d640d5252c
JoniSalonen <101516095+JoniSalonen@users.noreply.github.com>
2024-03-05 14:54:40 +0200

    testing and new code

--- hello.cpp
+++ hello.cpp
@@ -7,5 +7,7 @@ int main()
         string hello = "Hello";
         string devops = "DevOps";
         string lab = "lab 6";
+        string saying = "It worked i think";
         cout<< hello+ " " + devops+ " " + lab << endl;
+        cout << saying <<endl;
 }
